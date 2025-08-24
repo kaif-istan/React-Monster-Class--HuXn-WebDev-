c7ad959c13f460fb206752b79d86bfcc52abba35
kaif85077 <md.20213010@mnnit.ac.in>
2025-08-24 16:20:57 +0530

    context with use api

---
+++ React-19-Fundamentals/src/components/ThemeProvider.tsx
@@ -0,0 +1,18 @@
+import ThemeContext, { type Theme } from "./ThemeContext";
+import { useState, type ReactNode } from "react";
+
+const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
