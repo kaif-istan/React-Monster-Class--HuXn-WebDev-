2f38087b5cd1b56920b8f52cc2204b637cbacde3
kaif85077 <md.20213010@mnnit.ac.in>
2025-08-24 14:45:25 +0530

    Fetch data using use( . . . ) api.

--- React-19-Fundamentals/src/components/FetchTodo.tsx
+++ React-19-Fundamentals/src/components/FetchTodo.tsx
@@ -1,13 +1,23 @@
 import { use } from "react";
 // npm i react@experimental react-dom@experimental
 
-const fetchData = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-  return res.json();
-};
+// // create a fetch data function
+// const fetchData = async () => {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+//   return res.json();
+// };
+
+// // create a promise once outside the component
+// const todoPromise = fetchData();
+
+
+// Another way to create promise without async/await
+// use fetch.then()
+const todoPromise = fetch("https://jsonplaceholder.typicode.com/todos/1").then(res => res.json())
+
 
 const FetchTodo = () => {
-  const data = use(fetchData());
+  const data = use(todoPromise);
   return <h1>{data.title}</h1>;
 };
 
