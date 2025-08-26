791bd6462e47daece778ade836ee036f03ac0fc2
kaif85077 <md.20213010@mnnit.ac.in>
2025-08-26 21:19:59 +0530

    stagger animation completed.

--- 07_Framer-Motion/Framer-Motion-Basics/src/App.tsx
+++ 07_Framer-Motion/Framer-Motion-Basics/src/App.tsx
@@ -1,44 +1,54 @@
+import { useState } from "react";
 import { motion } from "motion/react";
 
-const images = [
-  {
-    src: "https://images.unsplash.com/photo-1446034295857-c39f8844fad4?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    caption: "Forest",
-  },
-  {
-    src: "https://images.unsplash.com/photo-1519681393784-d120267933ba?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    caption: "Mountains",
-  },
-  {
-    src: "https://images.unsplash.com/photo-1559666126-84f389727b9a?q=80&w=3877&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    caption: "Snow",
-  },
+const galleryImages = [
+  "https://images.unsplash.com/photo-1523712999610-f77fbcfc3843?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1506744038136-46273834b3fb?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1426604966848-d7adac402bff?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1490682143684-14369e18dce8?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1445964047600-cdbdb873673d?q=80&w=3784&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 ];
 
+const parentVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.5,
+      staggerDirection: 1,
+    },
+  },
+};
+
+const childVariants = { hidden: { opacity: 0 }, visible: { opacity: 1 } };
+
 const App = () => {
+  const [showImages, setShowImages] = useState(false);
+
   return (
-    <div className="min-h-screen bg-gray-400 flex items-center justify-center">
-      <div className="w-[80%] flex gap-5">
-        {images.map((image, index) => (
-          <motion.div key={index}
-          className="relative m-[1rem] overflow-hidden rounded-lg shadow-lg"
-          whileHover={{scale: 1.05}}
-          >
-            <img
-              src={image.src}
-              alt={image.caption}
-              className="w-full h-auto"
-            />
-            <motion.div 
-            className="absolute inset-0 flex items-center justify-center bg-black opacity-0 text-white duration-300 cursor-pointer"
-            whileHover={{opacity: 0.5}}
-            transition={{duration: 0.2, ease: 'easeInOut'}}
-            >
-              <p className="text-lg">{image.caption}</p>
-            </motion.div>
-          </motion.div>
+    <div>
+      <button
+        onClick={() => setShowImages(!showImages)}
+        className="mb-[2rem] p-6 rounded bg-yellow-500 text-black font-bold "
+      >
+        {showImages ? "Hide" : "Show"}
+      </button>
+      <motion.div
+        variants={parentVariants}
+        initial="hidden"
+        animate={showImages ? "visible" : "hidden"}
+        className="flex"
+      >
+        {galleryImages.map((img, idx) => (
+          <motion.img
+            key={idx}
+            src={img}
+            alt={`Gallery Image ${idx + 1}`}
+            variants={childVariants}
+            className="ml-[2rem] w-[300px] rounded"
+          />
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 };
